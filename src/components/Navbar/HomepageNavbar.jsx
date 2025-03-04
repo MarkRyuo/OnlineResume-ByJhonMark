24953efc88c8abc1ac2a444e5f724179c964b25b
MarkRyuo <malupajhonmark3035@gmail.com>
2025-03-04 14:35:54 +0800

    Subject: Update

--- src/components/Navbar/HomepageNavbar.jsx
+++ src/components/Navbar/HomepageNavbar.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 function HomepageNavbar() {
   return (
     <div>
+    <h1>Animoda Studio</h1>
       <ul>
         <li>Home</li>
-        <li></li>
+        <li>About</li>
       </ul>
     </div>
   )
