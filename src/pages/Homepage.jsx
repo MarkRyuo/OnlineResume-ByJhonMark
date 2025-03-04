530214eeaae765943c2bbd8cf3d05f8bacc32a27
MarkRyuo <malupajhonmark3035@gmail.com>
2025-03-04 15:44:03 +0800

    Subject: Update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import HomepageNavbar from '../components/Navbar/HomepageNavbar';
 
 function Homepage() {
   return (
-    <div style={{position: 'relative', width: '100vw', border: '1px solid red'}}>
+    <div style={{position: 'relative'}}>
       <HomepageNavbar />
     </div>
   )
