d3117a643d01ba45ae952506cc1071153ff3f5e0
MarkRyuo <malupajhonmark3035@gmail.com>
2025-03-06 15:51:43 +0800

    Subject: Update

--- src/components/Navbar/HomepageNavbar.jsx
+++ src/components/Navbar/HomepageNavbar.jsx
@@ -4,14 +4,8 @@ import '../../style/NavbarStyle/HomepageNavbar.css'
 function HomepageNavbar() {
   return (
     <div className='navbar-container'>
-      <div className='left-container'>
-        <div className='hamburger-menu'>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+      
         <h1>Animoda Studio</h1>
-      </div>
       <div className='buttons-container'>
         <button>
           <span>👌</span>
